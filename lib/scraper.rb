2a0144005f7e3b1eff556d794c048161460aa3a0
Michael Iodice <miodice3@gmail.com>
2020-10-21 10:37:23 -0600

    backup push before reconfiguring and consolidating print methods to remove one of the 3 print methods and call same code multiple times instead of calling individual print functions for each of the 3 print situations

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,28 +3,25 @@ class Scraper
     attr_accessor :base_url, :base_url_review
 
     def initialize
-        @base_url = "https://www.traillink.com/top-trails/"
-        @base_url_review = "https://www.traillink.com"
-        #base_url just traillink.com/ >> add top trails where you need it.
-        #url to query, 2nd function to parse information.
+        @base_url = "https://www.traillink.com"
     end
     
     def first_scrape
-       html = open(@base_url)
+       html = open(@base_url + "/top-trails/")
        trails_parsed_to_html = Nokogiri::HTML(html)
-       short=trails_parsed_to_html.css('#trail')        #make request to HTML webpage.
+       short=trails_parsed_to_html.css('#trail')        #make request to HTML webpage. short are the bundle of all trails
                                 #html elements selector query, TR within TB, for ID trails.
 
        trail_class_create_array = []
        
-        short.each do |each_trail|
+        short.each do |each_trail|                                  #iterate over each of the all trails bundle
             trail_name = each_trail.css("h3").text
             state = each_trail.css(".states").text
             length_mi = each_trail.css(".length").text.split[0]
             trail_desc_short = each_trail.css("div")[2].text
             trail_url = each_trail.css(".info").css("a").attr('href').text
 
-            state_instance = State.find_or_create_by_name(state)
+            state_instance = State.find_or_create_by_name(state)    #pass in state string, return instance of state
             
             trail_class_create_array = {trail_name_key: trail_name, state_key: state_instance, length_mi_key: length_mi, trail_desc_short_key: trail_desc_short, trail_url_key: trail_url}
             Trails.new(trail_class_create_array)
@@ -35,9 +32,7 @@ class Scraper
         comb_str = @base_url_review + string
         html = open(comb_str)
         trail_parsed_to_html = Nokogiri::HTML(html)
-        short = trail_parsed_to_html
-        trail_review_parking_info = short.css('p').text
-        trail_review_parking_info
+        trail_parsed_to_html.css('p').text #returns summary & parking paragraphs
     end
 
 end
